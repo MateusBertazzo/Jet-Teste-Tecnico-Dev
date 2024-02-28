a22775cc1b87af3b04d2670b7106000d4c55fc7f
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-28 19:43:25 -0300

    deixando table-desktop responsivel

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import fakeData from "./fakeData/fakeData";
 
 export default function Home() {
   return (
-    <div className="relative w-full flex flex-col shadow-lg mb-6 mt-10">
-      <table className="border-collapse w-auto divide-y divide-gray-200" aria-label="Car Rentals">
+    <div className="relative w-full overflow-auto shadow hidden md:block">
+      <table className="w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
           <tr>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th scope="col" className="w-96 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               CAR
             </th>
             <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -28,6 +28,7 @@ export default function Home() {
         <tbody className="bg-white divide-y divide-gray-200">
           {fakeData.map((car, index) => (
             <tr key={index} className="hover:bg-gray-50">
+
               <td className="px-6 py-4 whitespace-nowrap">
                 <div className="flex items-center">
                   <div className="flex gap-1 items-center h-16 w-16">
@@ -39,8 +40,7 @@ export default function Home() {
               </td>
 
               <td className="px-6 py-4 whitespace-nowrap">
-                <label htmlFor={`reservation-${index}`} className="sr-only">Next Reservation</label>
-                <input id={`reservation-${index}`} type="date" className="w-full p-2 border border-gray-300 rounded-md" />
+                <input type="date" className="w-full p-2 border border-gray-300 rounded-md" />
               </td>
 
               <td className="px-6 py-4 whitespace-nowrap">
@@ -52,7 +52,7 @@ export default function Home() {
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">⭐️⭐️⭐️⭐️⭐️</td>
 
               <td className="flex px-6 py-10 whitespace-nowrap text-sm font-medium">
-                <button className="text-black hover:text-indigo-900" aria-label={`Reserve ${car.model}`}>Reserve</button>
+                <button className="text-black hover:text-indigo-900">Reserve</button>
               </td>
             </tr>
           ))}

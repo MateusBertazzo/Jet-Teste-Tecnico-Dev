73cd01e1eded3edf829d3aea90fa1459ea2049f9
Mateus Bertazzo <mateusbataglin52@gmail.com>
2024-02-28 16:54:21 -0300

    setup inicial: first commit

---
+++ app/fakeData/fakeData.ts
@@ -0,0 +1,15 @@
+interface Car {
+    id: number;
+    model: string;
+    year: number;
+  }
+  
+const fakeData: Car[] = [
+    { id: 1, model: "Mini Cooper", year: 2022 },
+    { id: 2, model: "Toyota Corolla", year: 2020 },
+    { id: 3, model: "Honda Civic", year: 2019 },
+    { id: 4, model: "Ford Mustang", year: 2021 },
+    { id: 5, model: "Tesla Model S", year: 2023 },
+];
+  
+export default fakeData;
